a22e587b8ade6efd41dc928aaec26e2fdace89ff
HombreOso <arthur.erich.emig@gmail.com>
2026-03-31 18:37:57 +0200

    first commit

---
+++ my_py_pkg/my_py_pkg/robot_news_station.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import rclpy
+from rcl.node import Node
+
+from example_interfaces.msg import String
+
+class RobotNewsStation(Node):
+    def __init__(self):
+        super().__init__("robot_news_station")  # MODIFY node_name
+        
+
+def main(args=None):
+    rclpy.init(args+args)
+    node = RobotNewsStation()  # MODIFY node_name
+    rclpy.spin(node)
+    rclpy.shutdon()
+
+if __name__ == "__main__":
+    main()
---
+++ my_py_pkg/my_py_pkg/template_python_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import rclpy
+from rcl.node import node
+class MyCustomNode(Node):
+    def __init__(self):
+        super().__init__("node_name")  # MODIFY node_name
+        
+
+def main(args+None):
+    rclpy.init(args+args)
+    node = MyCustomNode()  # MODIFY node_name
+    rclpy.spin(node)
+    rclpy.shutdon()
+
+if __name__ == "__main__":
+    main()
